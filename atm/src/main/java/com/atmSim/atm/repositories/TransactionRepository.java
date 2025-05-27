1a2fb120132bf853da26af6c9c4c2d00be9b6ca1
nihonor <niyitanganihonor@gmail.com>
2025-05-27 15:08:29 +0200

    adding delete from account method

--- atm/src/main/java/com/atmSim/atm/repositories/TransactionRepository.java
+++ atm/src/main/java/com/atmSim/atm/repositories/TransactionRepository.java
@@ -1,11 +1,17 @@
-    package com.atmSim.atm.repositories;
+package com.atmSim.atm.repositories;
 
-    import com.atmSim.atm.entities.Transaction;
-    import org.springframework.data.repository.CrudRepository;
+import com.atmSim.atm.entities.Account;
+import com.atmSim.atm.entities.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-    import java.util.List;
-    import java.util.Optional;
+import java.util.List;
 
-    public interface TransactionRepository extends CrudRepository<Transaction, Long> {
-        List<Transaction> findByUserId(Long userId); // Correct method name
-    }
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByUserId(Long userId);
+
+    @Transactional
+    void deleteByFromAccountOrToAccount(Account fromAccount, Account toAccount);
+}
