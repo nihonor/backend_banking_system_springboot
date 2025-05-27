8f938067a93454b6b5d56ff0f68a84d1026502a1
nihonor <niyitanganihonor@gmail.com>
2025-05-27 15:10:32 +0200

    adding creating a customer for the admin

--- atm/src/main/java/com/atmSim/atm/controller/UserController.java
+++ atm/src/main/java/com/atmSim/atm/controller/UserController.java
@@ -7,18 +7,26 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@CrossOrigin(allowCredentials = "true",origins = "http://localhost:5000" )
+@CrossOrigin(allowCredentials = "true", origins = "http://localhost:5000")
 @RequestMapping("/api/users")
 @SecurityRequirement(name = "bearerAuth")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Operation(summary = "Create new customer (Admin only)")
+    @PostMapping("/create-customer")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<User> createCustomer(@RequestBody User newUser) {
+        return ResponseEntity.ok(userService.createCustomer(newUser));
+    }
 
     @Operation(summary = "Get user by ID")
     @GetMapping("/{id}")
@@ -38,5 +46,24 @@ public class UserController {
         return ResponseEntity.ok(userService.createAccount(id, accountType));
     }
 
+    @Operation(summary = "Edit user details (Admin only)")
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<User> editUser(@PathVariable Integer id, @RequestBody User updatedUser) {
+        return ResponseEntity.ok(userService.editUser(id, updatedUser));
+    }
 
+    @Operation(summary = "Suspend user (Admin only)")
+    @PutMapping("/{id}/suspend")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<User> suspendUser(@PathVariable Integer id) {
+        return ResponseEntity.ok(userService.suspendUser(id));
+    }
+
+    @Operation(summary = "Activate user (Admin only)")
+    @PutMapping("/{id}/activate")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<User> activateUser(@PathVariable Integer id) {
+        return ResponseEntity.ok(userService.activateUser(id));
+    }
 }
